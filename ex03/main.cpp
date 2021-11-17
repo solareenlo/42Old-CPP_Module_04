83729d46d05b7c5e5619ade73c154a5cdea0784b
solareenlo <solareenlo@gmail.com>
2021-11-17 12:02:00 +0900

    Fix: ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 03:30:41 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/11/17 11:16:47 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/11/17 11:52:44 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -29,13 +29,15 @@ int main() {
     ICharacter* me = new Character("me");
     std::cout << std::endl;
 
-    std::cout << "< Create materia >" << std::endl;
+    std::cout << "< Create & equip materia >" << std::endl;
+    AMateria* ice = src->createMateria("ice");
+    me->equip(ice);
     AMateria* tmp;
-    tmp = src->createMateria("ice");
-    me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
     tmp = src->createMateria("fire");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
     me->equip(tmp);
     std::cout << std::endl;
 
@@ -44,6 +46,18 @@ int main() {
     me->use(0, *bob);
     me->use(1, *bob);
     me->use(2, *bob);
+    me->use(3, *bob);
+    me->use(100, *bob);
+    std::cout << std::endl;
+
+    std::cout << "< Unequip materia >" << std::endl;
+    me->unequip(0);
+    delete ice;
+    me->use(0, *bob);
+    me->use(1, *bob);
+    me->use(2, *bob);
+    me->use(3, *bob);
+    me->unequip(100);
     me->use(100, *bob);
     std::cout << std::endl;
 
